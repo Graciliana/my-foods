84eb7c1bf81e566c1742aa84d167dc1c7cc63332
Graciliana Kascher <eagraciliana@gmail.com>
2024-05-09 18:18:46 -0300

    :lipstick: [refactor]: modified color and margin

--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -4,7 +4,7 @@ import RestaurantItem from "./restaurant-item";
 const RestaurantList = async () => {
   const restaurants = await db.restaurant.findMany({ take: 10 });
   return (
-    <div className=" flex overflow-x-scroll gap-4 py-4  drop-shadow-xl ">
+    <div className=" flex overflow-x-scroll gap-4 py-4  drop-shadow-xl px-5  ">
       {restaurants.map((restaurant) => (
         <RestaurantItem key={restaurant.id} restaurant={restaurant} />
       ))}
--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -5,7 +5,10 @@ import { SearchIcon } from "lucide-react";
 export const Search = () => {
   return (
     <div className="flex gap-2 drop-shadow-xl max-w-full">
-      <Input placeholder="Buscar restaurantes" className="border-none" />
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none bg-[#f4f4f5]"
+      />
       <Button size="icon" className="bg-[#ea1d2c]">
         <SearchIcon size={20} />
       </Button>
