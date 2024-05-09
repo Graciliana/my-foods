8b1593f6afe3fe6b44a7fb3aea306f841c68afde
Graciliana Kascher <eagraciliana@gmail.com>
2024-05-09 15:10:23 -0300

    :recycle: [refacfor]: ensure product list recives products as props

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -1,22 +1,20 @@
+import { Prisma } from "@prisma/client";
 import { db } from "../_lib/prisma";
 import ProductItem from "./product-item";
 
-export const ProductList = async () => {
-  const products = await db.product.findMany({
-    where: {
-      discountPercentage: {
-        gt: 0,
-      },
-    },
-    take: 10,
+interface ProductListProps {
+  products: Prisma.ProductGetPayload<{
     include: {
       restaurant: {
         select: {
-          name: true,
-        },
-      },
-    },
-  });
+          name: true;
+        };
+      };
+    };
+  }>[];
+}
+
+export const ProductList = async ({ products }: ProductListProps) => {
   return (
     <div className=" flex overflow-x-scroll gap-4 py-4  drop-shadow-xl ">
       {products.map((product) => (
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import { ChevronRight } from "lucide-react";
 import { CategoryList } from "./_components/category-list";
 import { Header } from "./_components/header";
 import { PromoBanner } from "./_components/promo-banner";
 import { Search } from "./_components/search";
-export default function Home() {
+import { Button } from "./_components/ui/button";
+import { ProductList } from "./_components/product-list";
+
+import { db } from "./_lib/prisma";
+
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <div className="px-5 pt-6">
@@ -20,6 +42,28 @@ export default function Home() {
           alt="Até 30% de desconto em pizzas!"
         />
       </div>
+      <div className="pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="text-[#323232] font-semibold text-base ">
+            Pedidos Recomendados
+          </h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 font-norma text-red-600 hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRight size={16} />
+          </Button>
+        </div>
+        <ProductList products={products} />
+      </div>
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/BannerBurguer-02.svg"
+          alt="A partir de R$17,90 em lanches"
+        />
+      </div>
     </>
   );
-}
+};
+export default Home;
